106ae6cf7e6ab8b6026758dd6d88367177c74ae5
Amit <kumarsamit335@gmail.com>
2022-07-15 16:58:13 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { SigninComponent } from './component/signin/signin.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './common-component/header/header.component';
 
 
 
@@ -33,7 +34,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MovieDetailsComponent,
     Error404Component,
     SigninComponent,
-    SignupComponent,  
+  HeaderComponent,
+    SignupComponent,
 
     
     
